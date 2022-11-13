98cd2ac931713f32f1870633f0ac7531ce6873f2
Tyler Yablonovsky <tyleryablonovsky@Tylers-MacBook-Pro.local>
2022-11-13 00:59:31 -0700

    got the link list working

--- content.js
+++ content.js
@@ -1,22 +1,56 @@
 // this file is where you can change certain page elements. So this is where I am grabbing all the links
 
+// const linkMessage = document.getElementById('message');
+
+// console.log("link message: ", linkMessage);
+
+
+
+
+
+
+var LinkList = [];
 function getLinks(){
     const Links = document.getElementsByTagName('a');
-    var LinkList = [];
+    console.log("all link info: ",Links);
+    //var LinkList = [];
 
-    for (Link in Links){
-        console.log(Link);
-        LinkList.push(Links[Link].href);
+    for (let Link in Links){
+        //console.log(Link);
+        LinkList.push({link: Links[Link].href, html: Links[Link].innerHTML});
     }
     //this doesnt show in the extention console. It shows in the browser console.
     // so next step would be to figure out how to get this "LinkList" variable to the script.js file
-    console.log('list: ', LinkList);
+    console.log('list in content: ', LinkList);
+    //linkMessage.innerHTML = LinkList;
     return LinkList;
 }
 
 //console.log("links: ", Links);
 
-// module.exports = {LinkList};
 
-//chrome.storage.local.set({LinksList: LinkList});
+
+
 chrome.runtime.sendMessage(getLinks());
+
+// chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+//     if (message === 'get-links'){
+//         sendResponse(LinkList);
+//     }
+//     else{
+//         sendResponse("not right message");
+//     }
+// });
+
+
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
+    console.log("reqParams: ", request, sender, sendResponse);
+    console.log("linklist before sending it: ", LinkList);
+    sendResponse(LinkList);
+});
+
+
+
+// export default getLinks;
+
+//module.exports = getLinks;
--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 // this currently isn't working. it won't pull the variable from content.js
 // var http = require('http');
-// const {LinkList} = require('./content.js');
+//const { LinkList } = require('./content.js');
+// import {links} from './content.js';
+// import LinkList from "./content.js";
+
+// import getLinks from './content.js';
+// const links = getLinks;
+
+//const getLinks = require('./content');
+// import getLinks from './content';
+
+// import { getLinks } from "./content";
+// console.log(getLinks);
 
 //buttons
 const imageButton = document.getElementById('image-button');
@@ -8,16 +19,23 @@ const linkButton = document.getElementById('link-button');
 //pages
 const homepage = document.getElementById('homescreen');
 const loadingPage = document.getElementById('loading');
+const loadingMessage = document.getElementById('loadingMessage');
+const linkSuccess = document.getElementById('linkSuccess');
+const linkError = document.getElementById('linkError');
+const Blinks = document.getElementById('insertBlinks');
 
-const fullLinkList = document.body.getElementsByTagName("a");
+var LinkList = [];
+var BrokenLinks = [];
 
-var pageHTML = null;
-var pel = false;
-var firstA = false
-var isATag = false;
+// const fullLinkList = document.body.getElementsByTagName("a");
 
-var listOfLinks = [];
-var tempLink = "";
+// var pageHTML = null;
+// var pel = false;
+// var firstA = false
+// var isATag = false;
+
+// var listOfLinks = [];
+// var tempLink = "";
 
 document.querySelector('#link-button').onclick = function () {
     linkFunction();
@@ -31,31 +49,96 @@ document.querySelector('#image-button').onclick = function (){
 
 
 
-function linkFunction(){
+async function linkFunction(){
   homepage.style.display = "none";
   loadingPage.style.display = "flex";
-  console.log("here");
+  console.log("here in link function");
+  for (let i in LinkList){
+    console.log("individul: ", LinkList[i].link);
+    if ( LinkList[i].link != null){
+        var status = await makeRequest(LinkList[i].link);
+    }
+    if (status != '200'){
+        console.log("status is: ", status);
+        BrokenLinks.push(LinkList[i]);
+    }
+  }
+  console.log("broken links: ", BrokenLinks);
+  console.log("B list length: ", BrokenLinks.length);
+
+  if (BrokenLinks.length == 0){
+    console.log("link scanner success. found no broken links");
+    loadingMessage.style.display = "none";
+    linkSuccess.style.display = "block";
+    loadingPage.style.alignItems = "flex-start";
+  } else {
+    console.log("there was broken links");
+    for (let i in BrokenLinks){
+        addElement(BrokenLinks[i].link, BrokenLinks[i].html);
+    }
+
+    loadingMessage.style.display = "none";
+    linkError.style.display = "block";
+    loadingPage.style.alignItems = "flex-start";
+    
+  }
 
 
-  chrome.runtime.onMessage.addListener(
-      // console.log("here again");
-      function(response, sender, sendResponse){
-          console.log("here again");
-          var listOfLinks = response;
-          console.log("list of links: ", listOfLinks);
-          return listOfLinks;
-      }
-  )
+
 }
+
+function addElement(link, HtMl){
+    console.log("url: ", link);
+    console.log("text: ", HtMl);
+    const newDiv = document.createElement("div");
+    const newContent = document.createElement("p");
+    newContent.innerHTML = "link: ";
+    newContent.style.color = "#EB6565";
+    const urlContent = document.createElement("p");
+    urlContent.innerHTML = link;
+    urlContent.style.paddingLeft = "8px";
+    urlContent.style.color = "#EB6565";
+
+    newDiv.appendChild(newContent);
+    newDiv.appendChild(urlContent);
+
+    newDiv.style.display = "flex";
+    newDiv.style.marginBottom = "10px";
+
+    Blinks.appendChild(newDiv);
+}
+
+
+async function makeRequest(url){
+    try {
+        var response = await fetch(url);
+        // console.log('response.status: ', response.status);
+        // console.log(response);
+        return response.status;
+    } catch (err) {
+        console.log(err);
+    }
+}
+
 
 function imageFunction(){
     homepage.style.display = "none";
     loadingPage.style.display = "flex";
     // make the list of images here
+
 }
 
-
-
+var Links = [];
+chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, {greeting: "hello"}, function(response) {
+        console.log("tabs res: ", response);
+        LinkList = response;
+    });
+});
+// chrome.runtime.sendMessage('get-links', (response) => {
+//     console.log("in runtime, links are: ", response);
+//     Links = response;
+// });
 
 //dont know if we need this code any more becuase it gave me the html but in string format... so not the best
 
